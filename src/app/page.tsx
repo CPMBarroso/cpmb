7ea80556e814aeac080c0dfe416d61ba5ffdc4ef
CPMBarroso <122379687+CPMBarroso@users.noreply.github.com>
2023-09-30 11:41:36 +0100

    *

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-import Image from 'next/image'
+import Particles from "@components/particles"
 
 export default function Home() {
   return (
-<div>main</div>
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gradient-to-tl from-black via-zinc-600/20 to-black overflow-hidden ">
+      <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
+      <h1 className="
+        text-4xl sm:text-6xl md:text-9xl text-transparent text-edge-outline
+        bg-white
+        bg-clip-text
+        cursor-default
+        animate-title duration-1000 
+        whitespace-nowrap
+        z-10"
+        >
+        cpmb
+      </h1>
+    </div>
   )
 }
