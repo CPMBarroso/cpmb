28e416bb4f1a3db2f8795f08b78cfe1a386f3190
CPMBarroso <122379687+CPMBarroso@users.noreply.github.com>
2023-10-01 01:21:20 +0100

    *

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,10 +1,16 @@
 import Footer from "@components/footer/footer"
 import Header from "@components/header/header"
 import Particles from "@components/particles"
+import localFont from 'next/font/local'
+
+const poppins = localFont({
+	src: "../../assets/fonts/Poppins/Poppins-Bold.woff",
+	variable: "--font-poppins",
+});
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative flex flex-col min-h-screen bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900">
+    <div className={`${poppins.className} relative flex flex-col min-h-screen bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900`}>
       <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
       <Header />
       <main
@@ -21,3 +27,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     </div>
   )
 }
+function LocalFont(arg0: { src: string; variable: string; }) {
+  throw new Error("Function not implemented.");
+}
+
