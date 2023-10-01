d09032a8c30b180a974d4488577af174255f0670
CPMBarroso <122379687+CPMBarroso@users.noreply.github.com>
2023-10-01 02:59:00 +0100

    *

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <link rel="preload" as="font" href="/assets/fonts/Poppins/Poppins-Bold.woff"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
